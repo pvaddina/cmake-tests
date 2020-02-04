6aca44a6a9441374a49e07eeb8897251247a6c70
Prakash Rao Vaddina <prakash.vaddina@gmail.com>
2020-02-04 08:21:19 +0100

    Rearranging the directories but the install of versionc config fails

--- install/build.sh
+++ install/build.sh
@@ -1,8 +1,9 @@
-rm -rf gen
+cmakeInstallPrefix="../../../gen/"
+rm -rf ../gen
 rm -rf calc_lib/build
 mkdir calc_lib/build
 cd calc_lib/build
-cmake -DCMAKE_INSTALL_PREFIX=../../ ..
+cmake -DCMAKE_INSTALL_PREFIX=$cmakeInstallPrefix ..
 make
 make install
-cd ../..
+cd ../../
