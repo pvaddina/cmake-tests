105d86767d88ec83ed39cc6cdc5a029cd06def4c
Prakash Rao Vaddina <prakash.vaddina@gmail.com>
2020-02-12 23:41:35 +0100

    lib generating and lib consuming examples

--- 01_consumer/build.sh
+++ 01_consumer/build.sh
@@ -1,7 +1,11 @@
-cmakeModPath="../../../gen/"
+cmakeDependencyPath="../../gen/calc_lib/cmake"  # Relative to the current 
+cmakeInstallPath="../../../gen" # Relative to the build directory
 
+rm -rf ../gen/calculator
 rm -rf calculator/build
 mkdir calculator/build
 cd calculator/build
-cmake -DCMAKE_MODULE_PATH=$cmakeModPath ..
+#cmake -DCMAKE_PREFIX_PATH=$cmakeDependencyPath -DCMAKE_INSTALL_PREFIX=$cmakeInstallPath ..
+cmake -DCMAKE_PREFIX_PATH=$cmakeDependencyPath -DCMAKE_INSTALL_PREFIX=$cmakeInstallPath ..
 make
+make install
